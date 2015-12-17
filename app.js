6dbcf94bdb4c61965a4767973b0c1074b74f3060
Robby Newman <robbynewman@Robbys-MacBook-Pro.local>
2015-12-17 10:57:08 -0600

    first commit

--- app.js
+++ app.js
@@ -1,61 +1,58 @@
+var cohortArray = [ "scott", "kristy", "natalie", "charlie", "sam", "zach", "nathan", "paul", "anthony", "liz", "eric", "jeremy", "chris", "brooks", "altamir", "amber", "joe", "kenzie", "matthew", "robby", "mark","scott"];
 
 
 $(document).ready(function(){
-	
-	
 	// When the page loads, have a button on the DOM that says 
 	// 'generate'.
-	$('#container').append("<button class = 'generate-click'>Generate</button>");
+	for (var i = 2; i < cohortArray.length/2; i++) {
+	
+		$(".numbers").append("<button>" + i + "</button>");
+	}
 
-	//click handlers is what I think these are called
-	$('#container').on('click','.generate-click',generateClick);
-	$('#container').on('click', '.delete-click', deleteClick);
-	$('#container').on('click', '.change-click', changeClick);
+$('.numbers').on('click', 'button',  numberClick)
+$('.buttonContainer').on('click', '.randomizer', genGroup)
 
 });
-//Upon clicking on that button, append a new container onto the DOM.
+	
 
-//Additionally, 
+var num = 0;
 
-
-// To keep track of how many times the Generate button is clicked
-var counter = 0;
-
-
-function generateClick() {
-	// To keep track of how many times the Generate button is clicked
-	counter++;
-
-	//append a new container to the dom. There should be text that provides a number.
- 	//The number should be the number of times the 'generate row' button has been clicked.
-	$('#container').append("<div class='normal'> <p>" + counter + 
-
-		//That container should have two buttons, one that reads 'delete', the other that reads 'change'. 
-		"</p><button class = 'delete-click'> Delete </button>" +
-		"<button class='change-click'> Change </button></div>");
-
-};
-
-
-// The delete button should delete JUST the container that the button is in. 
-function deleteClick() {
-	$(this).parent().remove();
-}
-
-// The change button should change the background-color of the container to red. 
-// The default or 'normal' state of the background-color should be yellow. Clicking 
-// the button a second time should change it back to yellow, 3rd time red, 4th time yellow, etc. 
-
-function changeClick() {
-	$(this).parent().toggleClass("active");
+function numberClick () {
+	num = $(this).text();
+	console.log(num)
 }
 
 
+function genGroup () {
+	
 
-// Once you are complete, check your work into a new repo and post it on GitHub. 
-// Then head over to this form and fill in the info:
-// http://goo.gl/forms/hCGLfyz8YK
+	var groupMembers = cohortArray.length/num;
+	var selected = [];
+	for (var i = 0; i < cohortArray.length; i++) {
+		selected[i]=cohortArray[i];
 
+	}
+	console.log(selected);
+	$(".container").children().remove();
+	for (var i = 1; i <= num; i++) {
 
+		$(".container").append("<div class='group' id=" + i + ">Group " + i + "</div>");
+		for (var j = 1; j < groupMembers; j++) {
+			var person = randomNumber(0, selected.length);
+			if (selected[person]) { 
+				$("#"+i).append("<p>" + selected[person] + "</p>");
+			selected.splice(person, 1);
+			console.log("selected: " , selected);
+			console.log("cohort ", cohortArray);
+			}
+			
+		}
 
+	}
+	//console.log("selected: " , selected);
+		
+}
 
+function randomNumber(min, max) {
+	return Math.floor(Math.random() * (1 + max - min) + min);
+}
